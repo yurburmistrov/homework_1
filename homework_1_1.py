2539893a445fa552c3ee3c0de7afd1f9fdc6f377
Yury <yurburmistrov@gmail.com>
2020-12-09 22:11:38 +0300

    First H-W

--- homework_1_1.py
+++ homework_1_1.py
@@ -1,1 +1,15 @@
-# ПЕрвая домашка
+# Первый раздел
+sister = "Варя"
+number_hotel = 2
+bank_percent = 2.5
+print (bank_percent, sister, number_hotel)
+
+personal_age = int(input ("Введите, пожалуйста, свой возраст:\n >>>"))
+print (f'Здравствуйте, Ваш возраст {personal_age} лет')
+
+granny_name=input("Укажите имя вашей бабушки \n >>>")
+print(f'Имя вашей бабушки {granny_name}')
+
+
+
+
